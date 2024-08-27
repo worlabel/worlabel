fe3a5bda9c9125878a1ae23aecda996eff23bac4
jhynsoo <jhynsoo@gmail.com>
2024-08-27 14:33:10 +0900

    Chore: shadcn/ui 설정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,12 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), svgr()],
   base: './',
   test: {
     globals: true,
