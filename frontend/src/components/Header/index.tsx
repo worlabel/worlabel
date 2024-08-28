dd0bdfbd5c50d878c501518a8d94a75f581932c2
정현조 <j96263732@gmail.com>
2024-08-28 09:17:50 +0900

    Feat : Header 컴포넌트 ui 구현 - S11P21S002-61

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import { cn } from '@/lib/utils';
+import bellIcon from '@/assets/icons/bell.svg';
+import userIcon from '@/assets/icons/user.svg';
+
+export interface HeaderProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export default function Header({ className, ...props }: HeaderProps) {
+  return (
+    <header
+      className={cn(
+        'flex h-16 w-full items-center justify-between border-b px-4 sm:px-6 md:px-8 lg:px-10',
+        'bg-color-background-default-default border-color-border-default-default',
+        className
+      )}
+      {...props}
+    >
+      <div className="flex items-center gap-4 md:gap-10">
+        <div
+          className={cn('text-[20px] font-normal tracking-[-1.60px] text-black sm:text-[24px] md:text-[32px]')}
+          style={{ fontFamily: "'Offside-Regular', Helvetica" }}
+        >
+          WorLabel
+        </div>
+        <nav className="items-center hidden gap-5 md:flex">
+          <div className={cn('text-color-text-default-default', 'font-body-strong', 'text-sm sm:text-base md:text-lg')}>
+            workspace
+          </div>
+          <div className={cn('text-color-text-default-default', 'font-body', 'text-sm sm:text-base md:text-lg')}>
+            labeling
+          </div>
+        </nav>
+      </div>
+      <div className="flex items-center gap-4 md:gap-5">
+        <img
+          className="w-4 h-4 sm:h-5 sm:w-5"
+          src={bellIcon}
+          alt="Bell Icon"
+        />
+        <img
+          className="w-4 h-4 sm:h-5 sm:w-5"
+          src={userIcon}
+          alt="User Icon"
+        />
+      </div>
+    </header>
+  );
+}
