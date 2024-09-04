4c6e04d3caabc4c18308162c25eede862ad8995b
jhynsoo <jhynsoo@gmail.com>
2024-09-04 15:53:26 +0900

    Feat: 캔버스 컨트롤 바 컴포넌트 구현 - S11P21S002-63

---
+++ frontend/src/components/CanvasControlBar/index.stories.tsx
@@ -0,0 +1,9 @@
+import '@/index.css';
+import CanvasControlBar from '.';
+
+export default {
+  title: 'Components/CanvasControlBar',
+  component: CanvasControlBar,
+};
+
+export const Default = () => <CanvasControlBar />;
