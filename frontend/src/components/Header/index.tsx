01baf090f297fee2f515897075060b17a88bdaac
정현조 <j96263732@gmail.com>
2024-09-09 14:58:12 +0900

    Refactor: 헤더가 메인 페이지일 때 네비게이트나 다른 아이콘이 안 뜨도록 함

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
 import { cn } from '@/lib/utils';
 import { Bell, User } from 'lucide-react';
+import { useLocation } from 'react-router-dom';
 
 export interface HeaderProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export default function Header({ className, ...props }: HeaderProps) {
+  const location = useLocation();
+
+  const isHomePage = location.pathname === '/';
+
   return (
     <header
       className={cn(
@@ -20,19 +25,27 @@ export default function Header({ className, ...props }: HeaderProps) {
         >
           WorLabel
         </div>
-        <nav className="hidden items-center gap-5 md:flex">
-          <div className={cn('text-color-text-default-default', 'font-body-strong', 'text-sm sm:text-base md:text-lg')}>
-            workspace
-          </div>
-          <div className={cn('text-color-text-default-default', 'font-body', 'text-sm sm:text-base md:text-lg')}>
-            labeling
-          </div>
-        </nav>
+
+        {!isHomePage && (
+          <nav className="hidden items-center gap-5 md:flex">
+            <div
+              className={cn('text-color-text-default-default', 'font-body-strong', 'text-sm sm:text-base md:text-lg')}
+            >
+              workspace
+            </div>
+            <div className={cn('text-color-text-default-default', 'font-body', 'text-sm sm:text-base md:text-lg')}>
+              labeling
+            </div>
+          </nav>
+        )}
       </div>
-      <div className="flex items-center gap-4 md:gap-5">
-        <Bell className="h-4 w-4 text-black sm:h-5 sm:w-5" />
-        <User className="h-4 w-4 text-black sm:h-5 sm:w-5" />
-      </div>
+
+      {!isHomePage && (
+        <div className="flex items-center gap-4 md:gap-5">
+          <Bell className="h-4 w-4 text-black sm:h-5 sm:w-5" />
+          <User className="h-4 w-4 text-black sm:h-5 sm:w-5" />
+        </div>
+      )}
     </header>
   );
 }
