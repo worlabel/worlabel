020c2f6739d00348b5e0a44d3549944ac57aca96
정현조 <j96263732@gmail.com>
2024-09-09 17:30:09 +0900

    Refactor: 라우터에 관리자 페이지 추가

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -3,13 +3,17 @@ import Home from '@/components/Home';
 import WorkspaceBrowseDetail from '@/components/WorkspaceBrowseDetail';
 import WorkspaceBrowseLayout from '@/components/WorkspaceBrowseLayout';
 import WorkspaceLayout from '@/components/WorkspaceLayout';
-
+import AdminLayout from '@/components/AdminLayout';
+import ReviewList from '@/components/ReviewList';
+import AdminMemberManage from '@/components/AdminMemberManage';
 import { createBrowserRouter } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 export const webPath = {
   home: () => '/',
   browse: () => '/browse',
   workspace: () => '/workspace',
+  admin: (id: string) => `/admin/${id}`,
 };
 
 const router = createBrowserRouter([
@@ -44,6 +48,24 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <div>workspace</div>,
+      },
+    ],
+  },
+  {
+    path: webPath.admin(':id'),
+    element: <AdminLayout />,
+    children: [
+      {
+        index: true,
+        element: <Navigate to="review" />,
+      },
+      {
+        path: 'review',
+        element: <ReviewList />,
+      },
+      {
+        path: 'members',
+        element: <AdminMemberManage />,
       },
     ],
   },
