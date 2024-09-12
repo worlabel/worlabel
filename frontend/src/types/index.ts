3a1257054801e3f3f109628b457af76ea76ffe70
정현조 <j96263732@gmail.com>
2024-09-12 10:57:52 +0900

    Feat: type 추가

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -33,3 +33,113 @@ export type Label = {
   type: 'polygon' | 'rect';
   coordinates: Array<[number, number]>;
 };
+
+export interface FolderRequestDTO {
+  title: string;
+  parentId: number;
+}
+
+export interface ChildFolderDTO {
+  id: number;
+  title: string;
+}
+
+export interface FolderResponseDTO {
+  id: number;
+  title: string;
+  images: ImageResponseDTO[];
+  children: ChildFolderDTO[];
+}
+
+export interface ImageResponseDTO {
+  id: number;
+  imageTitle: string;
+  imageUrl: string;
+  status: 'PENDING' | 'IN_PROGRESS' | 'NEED_REVIEW' | 'COMPLETED';
+}
+
+export interface ImageMoveRequestDTO {
+  moveFolderId: number;
+}
+
+export interface ImageStatusChangeRequestDTO {
+  labelStatus: 'PENDING' | 'IN_PROGRESS' | 'NEED_REVIEW' | 'COMPLETED';
+}
+
+export interface MemberResponseDTO {
+  id: number;
+  nickname: string;
+  profileImage: string;
+}
+
+export interface WorkspaceRequestDTO {
+  title: string;
+  content: string;
+}
+
+export interface WorkspaceResponseDTO {
+  id: number;
+  memberId: string;
+  title: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface WorkspaceListResponseDTO {
+  workspaceResponses: WorkspaceResponseDTO[];
+}
+
+export interface SuccessResponse<T> {
+  status: number;
+  code: number;
+  message: string;
+  data: T;
+  errors: CustomError[];
+  isSuccess: boolean;
+}
+export interface ProjectRequestDTO {
+  title: string;
+  projectType: 'classification' | 'detection' | 'segmentation';
+}
+export interface ProjectResponseDTO {
+  id: number;
+  title: string;
+  workspaceId: number;
+  projectType: 'classification' | 'detection' | 'segmentation';
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface ProjectListResponseDTO {
+  workspaceResponses: ProjectResponseDTO[];
+}
+
+export interface CustomError {
+  field: string;
+  code: string;
+  message: string;
+  objectName: string;
+}
+
+export interface ErrorResponse {
+  status: number;
+  code: number;
+  message: string;
+  data: CustomError;
+  errors: CustomError[];
+  isSuccess: boolean;
+}
+
+export interface BaseResponse<T> {
+  status: number;
+  code: number;
+  message: string;
+  data: T;
+  errors: CustomError[];
+  isSuccess: boolean;
+}
+
+export interface RefreshTokenResponseDTO {
+  accessToken: string;
+}
