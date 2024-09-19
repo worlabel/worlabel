4ddf0cf6cb924f23ff0b04ac6cfbe1094ff3bb40
김용수 <akys159357@naver.com>
2024-09-19 12:11:13 +0900

    Refactor: 기존 권한체크 메서드 제거

--- backend/src/main/java/com/worlabel/domain/participant/service/ParticipantService.java
+++ backend/src/main/java/com/worlabel/domain/participant/service/ParticipantService.java
@@ -17,43 +17,6 @@ public class ParticipantService {
     private final ParticipantRepository participantRepository;
 
     /**
-     * 프로젝트 수정 권한이 없으면 예외
-     *
-     * @param memberId  사용자 ID
-     * @param projectId 프로젝트 ID
-     */
-    public void checkEditorUnauthorized(final Integer memberId, final Integer projectId) {
-        Participant participant = participantRepository.findByMemberIdAndProjectId(memberId, projectId)
-            .orElseThrow(() -> new CustomException(ErrorCode.PARTICIPANT_EDITOR_UNAUTHORIZED));
-
-        if (!participant.getPrivilege().isEditeAuth()) {
-            throw new CustomException(ErrorCode.PARTICIPANT_UNAUTHORIZED);
-        }
-    }
-
-    /**
-     * 프로젝트 리뷰 상태 수정 권한이 없으면 예외
-     */
-    public void checkManagerUnauthorized(final Integer memberId, final Integer projectId) {
-        Participant participant = participantRepository.findByMemberIdAndProjectId(memberId, projectId)
-            .orElseThrow(() -> new CustomException(ErrorCode.PARTICIPANT_EDITOR_UNAUTHORIZED));
-
-        if (!participant.getPrivilege().isManagerAuth()) {
-            throw new CustomException(ErrorCode.PARTICIPANT_UNAUTHORIZED);
-        }
-    }
-
-    /**
-     * 프로젝트 View 권한이 없으면 예외
-     */
-    public void checkViewerUnauthorized(final Integer memberId, final Integer projectId) {
-        if (!participantRepository.existsByMemberIdAndProjectId(memberId, projectId)) {
-            throw new CustomException(ErrorCode.PARTICIPANT_UNAUTHORIZED);
-        }
-    }
-
-
-    /**
      * privilegeType보다 같거나 큰 권한인지 확인
      */
     public void checkPrivilegeUnauthorized(final Integer memberId, final Integer projectId, final PrivilegeType privilegeType) {
