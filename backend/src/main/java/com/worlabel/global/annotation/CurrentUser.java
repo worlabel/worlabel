36cec776bd60a7cce65d3b0032e0d57422798828
김용수 <akys159357@naver.com>
2024-09-19 13:59:02 +0900

    Fix: Swagger memberId 제외

--- backend/src/main/java/com/worlabel/global/annotation/CurrentUser.java
+++ backend/src/main/java/com/worlabel/global/annotation/CurrentUser.java
@@ -1,5 +1,7 @@
 package com.worlabel.global.annotation;
 
+import io.swagger.v3.oas.annotations.Parameter;
+
 import java.lang.annotation.Documented;
 import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
@@ -8,5 +10,6 @@ import java.lang.annotation.RetentionPolicy;
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
 @Documented
+@Parameter(hidden = true)
 public @interface CurrentUser {
 }
