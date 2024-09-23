c05369985aaf87fe79ff32b2be8b4d9409c7f43e
김용수 <akys159357@naver.com>
2024-09-23 21:53:24 +0900

    Feat: 학습 진행시 참여 테이블에 추가

--- backend/src/main/java/com/worlabel/global/cache/CacheKey.java
+++ backend/src/main/java/com/worlabel/global/cache/CacheKey.java
@@ -2,4 +2,8 @@ package com.worlabel.global.cache;
 
 public class CacheKey {
     public static String authenticationKey(int memberId) { return "authentication:" + memberId;}
+
+    public static String trainProgressKey() {
+        return "trainProgress";
+    }
 }
