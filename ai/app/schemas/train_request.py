05e9a2c03b4e9940d5718297f546fa3801bd1fd4
김진현 <kimjh9903@naver.com>
2024-09-23 09:46:42 +0900

    Refactor: 레이블 카테고리를 포함한 오토레이블링 구현 및 리팩토링

--- ai/app/schemas/train_request.py
+++ ai/app/schemas/train_request.py
@@ -1,7 +1,6 @@
 from pydantic import BaseModel, Field
 from typing import List, Optional, Union
 from schemas.predict_response import LabelData
-from schemas.predict_request import LabelCategory
 
 class TrainDataInfo(BaseModel):
     image_url: str
@@ -15,4 +14,3 @@ class TrainRequest(BaseModel):
     epochs: int = 50 # 훈련 반복 횟수
     batch: Union[float, int] = -1 # 훈련 batch 수[int] or GPU의 사용률 자동[float] default(-1): gpu의 60% 사용 유지
     path: Optional[str] = Field(None, alias="model_path")
-    label_categories: Optional[List[LabelCategory]] = None # 새로운 레이블 카테고리 확인용
