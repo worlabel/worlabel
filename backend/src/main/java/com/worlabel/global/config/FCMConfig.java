0ceeda6e6b493b977786f40eb108f01be938323a
김용수 <akys159357@naver.com>
2024-09-24 11:32:58 +0900

    Feat: FCM Json Key 추가

---
+++ backend/src/main/java/com/worlabel/global/config/FCMConfig.java
@@ -0,0 +1,14 @@
+package com.worlabel.global.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class FCMConfig {
+
+    // Firebase 설정 Json 파일의 경로
+}
