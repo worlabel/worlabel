7e786019068ac56af6cedd357316892628234f6e
kimtaesoo7 <kimtaseoo7@naver.com>
2024-09-25 17:37:03 +0900

    Feat: AI 데이터 형식 맞추기  S11P21S002-224

--- backend/src/main/java/com/worlabel/domain/result/entity/Optimizer.java
+++ backend/src/main/java/com/worlabel/domain/result/entity/Optimizer.java
@@ -5,13 +5,13 @@ import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum Optimizer {
 
-    AUTO("AUTO"),
+    AUTO("auto"),
     SGD("SGD"),
-    ADAM("ADAM"),
-    ADAMW("ADAMW"),
-    NADAM("NADAM"),
-    RADAM("RADAM"),
-    RMSPROP("RMSPROP");
+    ADAM("Adam"),
+    ADAMW("AdamW"),
+    NADAM("NAdam"),
+    RADAM("RAdam"),
+    RMSPROP("RMSProp");
 
     private final String value;
 
@@ -22,11 +22,11 @@ public enum Optimizer {
     @JsonCreator
     public static Optimizer from(String value) {
         for (Optimizer status : Optimizer.values()) {
-            if (status.getValue().equals(value.toUpperCase())) {
+            if (status.getValue().equalsIgnoreCase(value)) {
                 return status;
             }
         }
-        return null;
+        throw new IllegalArgumentException("Unknown optimizer: " + value);
     }
 
     @JsonValue
