7fcc0d9c2801cff696172afd0a2bd8cd259a5c0a
김진현 <kimjh9903@naver.com>
2024-09-25 21:24:30 +0900

    Feat: 에러핸들러 슬랙메세징 구현

--- ai/app/main.py
+++ ai/app/main.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.exception_handlers import http_exception_handler, request_validation_exception_handler
+from fastapi.exceptions import RequestValidationError
+from starlette.exceptions import HTTPException
 from api.yolo.detection import router as yolo_detection_router
 from api.yolo.segmentation import router as yolo_segmentation_router
 from api.yolo.model import router as yolo_model_router
+from utils.slackMessage import send_slack_message
 
 app = FastAPI()
 
@@ -9,6 +13,19 @@ app = FastAPI()
 app.include_router(yolo_detection_router, prefix="/api/detection", tags=["Detection"])
 app.include_router(yolo_segmentation_router, prefix="/api/segmentation", tags=["Segmentation"])
 app.include_router(yolo_model_router, prefix="/api/model", tags=["Model"])
+
+
+# 예외 처리기
+@app.exception_handler(HTTPException)
+async def custom_http_exception_handler(request:Request, exc):
+    body = await request.json()
+    send_slack_message(f"프로젝트 ID: {body['project_id']} - 실패! 에러: {str(exc)}", status="error")
+    return await http_exception_handler(request, exc)
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request:Request, exc):
+    send_slack_message(f"{request.url} - 요청 실패! 에러: {str(exc)}", status="error")
+    return await request_validation_exception_handler(request, exc)
 
 # # 애플리케이션 실행
 # if __name__ == "__main__":
