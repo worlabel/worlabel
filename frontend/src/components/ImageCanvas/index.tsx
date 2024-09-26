4da7a3e6f5e65fa26a65a68e59df7f66a6d74003
jhynsoo <jhynsoo@gmail.com>
2024-09-26 17:52:34 +0900

    Fix: 레이블 페이지 레이아웃 사이즈 수정

--- frontend/src/components/ImageCanvas/index.tsx
+++ frontend/src/components/ImageCanvas/index.tsx
@@ -20,7 +20,7 @@ export default function ImageCanvas() {
   const selectedLabelId = useCanvasStore((state) => state.selectedLabelId);
   const setSelectedLabelId = useCanvasStore((state) => state.setSelectedLabelId);
   const sidebarSize = useCanvasStore((state) => state.sidebarSize);
-  const stageWidth = window.innerWidth * ((100 - sidebarSize) / 100) - 200;
+  const stageWidth = window.innerWidth * ((100 - sidebarSize) / 100) - 201;
   const stageHeight = window.innerHeight - 64;
   const stageRef = useRef<Konva.Stage>(null);
   const dragLayerRef = useRef<Konva.Layer>(null);
