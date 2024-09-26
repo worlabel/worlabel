336cc33384ced85699e92e4a1152dec1c9478a1b
김진현 <kimjh9903@naver.com>
2024-09-26 17:20:37 +0900

    Refactor: train response 수정

---
+++ ai/app/schemas/train_response.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+
+class TrainResponse(BaseModel):
+    modelKey:str
+    precision: float
+    recall: float
+    mAP50: float
+    mAP5095: float
+    fitness: float
