8abb579076ba001dcc03970e48a4e211135063d2
정현조 <j96263732@gmail.com>
2024-09-26 19:23:52 +0900

    Comment: Todo 주석 추가

--- frontend/src/components/ModelManage/TrainingTab.tsx
+++ frontend/src/components/ModelManage/TrainingTab.tsx
@@ -7,7 +7,7 @@ import { ModelTrainRequest } from '@/types';
 interface TrainingTabProps {
   projectId: number | null;
 }
-
+//Todo : 로직 수정, isTrain을 서버 단에서 받고, 셀렉트 됐을 때 개별 조회로 isTrain을 판단해서, 학습 중이면 리패치하는 방식으로 관리한다.
 export default function TrainingTab({ projectId }: TrainingTabProps) {
   const numericProjectId = projectId ? parseInt(projectId.toString(), 10) : null;
   const { isTrainingByProject, setIsTraining, selectedModelByProject, setSelectedModel, resetTrainingData } =
