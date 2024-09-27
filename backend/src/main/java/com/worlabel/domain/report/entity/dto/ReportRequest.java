0d7cf263fc2f0141348b479e06b46284cd1c37fc
김용수 <akys159357@naver.com>
2024-09-27 17:58:23 +0900

    Refactor: 응답 수정

--- backend/src/main/java/com/worlabel/domain/report/entity/dto/ReportRequest.java
+++ backend/src/main/java/com/worlabel/domain/report/entity/dto/ReportRequest.java
@@ -1,5 +1,6 @@
 package com.worlabel.domain.report.entity.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,13 +11,29 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ReportRequest {
+
+    @JsonProperty("total_epochs")
     private int totalEpochs;
+
     private int epoch;
-    private double boxLoss;
-    private double clsLoss;
-    private double dflLoss;
+
     private double fitness;
+
+    @JsonProperty("epoch_time")
     private double epochTime;
+
+    @JsonProperty("left_seconds")
     private double leftSeconds;
+
+    @JsonProperty("box_loss")
+    private double boxLoss;
+
+    @JsonProperty("cls_loss")
+    private double clsLoss;
+
+    @JsonProperty("dfl_loss")
+    private double dflLoss;
+
+    @JsonProperty("seg_loss")
     private double segLoss;
 }
