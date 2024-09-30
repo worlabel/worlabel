798f313e81cf7ae9f1e0d788fe962addafc9b147
김진현 <kimjh9903@naver.com>
2024-09-30 12:02:38 +0900

    Feat: dotenv 이용하여 base_url 설정

--- ai/app/utils/api_utils.py
+++ ai/app/utils/api_utils.py
@@ -5,14 +5,15 @@ import os, httpx
 
 def send_data_call_api(project_id:int, model_id:int, data:ReportData):
     try:
-        # load_dotenv() 
-        # base_url = os.getenv("API_BASE_URL")
+        load_dotenv() 
+        base_url = os.getenv("API_BASE_URL")
         # main.py와 같은 디렉토리에 .env 파일 생성해서 따옴표 없이 아래 데이터를 입력
         # API_BASE_URL = {url}
         # API_KEY = {key}
 
         # 하드코딩으로 대체
-        base_url = "http://127.0.0.1:8080"
+        if not base_url:
+            base_url = "http://127.0.0.1:8080"
         
         headers = {
             "Content-Type": "application/json"
@@ -22,7 +23,8 @@ def send_data_call_api(project_id:int, model_id:int, data:ReportData):
             method="POST",
             url=base_url+f"/api/projects/{project_id}/reports/models/{model_id}",
             json=data.model_dump(),
-            headers=headers
+            headers=headers,
+            timeout=10
         )
         # status에 따라 예외 발생
         response.raise_for_status()
