532303e7779818f82ef26aa1a5ac94bff2658968
정현조 <j96263732@gmail.com>
2024-10-07 16:19:34 +0900

    Refactor: 파일 업로드 presinged 수정

--- frontend/src/components/WorkspaceDropdownMenu/index.tsx
+++ frontend/src/components/WorkspaceDropdownMenu/index.tsx
@@ -8,12 +8,6 @@ import {
   DropdownMenuTrigger,
 } from '../ui/dropdown-menu';
 import { Dialog, DialogContent, DialogHeader, DialogTrigger } from '../ui/dialogCustom';
-import ImageUploadForm from '../ImageUploadModal/ImageUploadForm';
-import ImageUploadPresignedForm from '../ImageUploadPresignedModal/ImageUploadPresignedForm';
-import useUploadImageFileQuery from '@/queries/projects/useUploadImageFileQuery';
-import useUploadImageFolderFileQuery from '@/queries/projects/useUploadImageFolderFileQuery';
-import useUploadImageZipQuery from '@/queries/projects/useUploadImageZipQuery';
-import useUploadImageFolderQuery from '@/queries/projects/useUploadImageFolderQuery';
 import ImagePreSignedForm from '../ImagePreSignedForm';
 
 export default function WorkspaceDropdownMenu({
@@ -25,25 +19,11 @@ export default function WorkspaceDropdownMenu({
   folderId: number;
   onRefetch: () => void;
 }) {
-  const [isOpenUploadFile, setIsOpenUploadFile] = React.useState<boolean>(false);
+  const [isOpenUpload, setIsOpenUpload] = React.useState<boolean>(false);
   const [fileCount, setFileCount] = React.useState<number>(0);
-  const [isOpenUploadPresigned, setIsOpenUploadPresigned] = React.useState<boolean>(false);
-  const [presignedCount, setPresignedCount] = React.useState<number>(0);
-  const [isOpenUploadFolderFile, setIsOpenUploadFolderFile] = React.useState<boolean>(false);
-  const [isOpenUploadFolder, setIsOpenUploadFolder] = React.useState<boolean>(false);
-  const [isOpenUploadZip, setIsOpenUploadZip] = React.useState<boolean>(false);
-  const [isOpenTestUpload, setIsOpenTestUpload] = React.useState<boolean>(false);
+  const [uploadType, setUploadType] = React.useState<'file' | 'folder' | 'zip'>('file');
 
-  const uploadImageZipMutation = useUploadImageZipQuery();
-  const uploadImageFolderFileMutation = useUploadImageFolderFileQuery();
-  const uploadImageFileMutation = useUploadImageFileQuery();
-  const uploadImageFolderMutation = useUploadImageFolderQuery();
-
-  const handleCloseUploadFile = () => setIsOpenUploadFile(false);
-  const handleCloseUploadFolderFile = () => setIsOpenUploadFolderFile(false);
-  const handleCloseUploadFolder = () => setIsOpenUploadFolder(false);
-  const handleCloseUploadZip = () => setIsOpenUploadZip(false);
-  const handleCloseTestUpload = () => setIsOpenTestUpload(false);
+  const handleCloseUpload = () => setIsOpenUpload(false);
 
   const handleFileCount = (fileCount: number) => {
     setFileCount(fileCount);
@@ -56,148 +36,59 @@ export default function WorkspaceDropdownMenu({
           <Menu size={16} />
         </DropdownMenuTrigger>
         <DropdownMenuContent className="w-56">
-          <DropdownMenuItem onClick={() => console.log('프로젝트 이름 수정')}>프로젝트 이름 수정</DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={() => {
+              setUploadType('file');
+              setIsOpenUpload(true);
+            }}
+          >
+            파일 업로드
+          </DropdownMenuItem>
           <DropdownMenuSeparator />
-          <DropdownMenuItem onClick={() => setIsOpenUploadFile(true)}>파일 업로드</DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setIsOpenUploadPresigned(true)}>
-            파일 업로드 (PresignedUrl 이용)
+          <DropdownMenuItem
+            onClick={() => {
+              setUploadType('folder');
+              setIsOpenUpload(true);
+            }}
+          >
+            폴더 업로드
           </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setIsOpenUploadFolderFile(true)}>
-            폴더 업로드 (파일 업로드 API 이용)
+          <DropdownMenuSeparator />
+          <DropdownMenuItem
+            onClick={() => {
+              setUploadType('zip');
+              setIsOpenUpload(true);
+            }}
+          >
+            압축 파일 업로드
           </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setIsOpenUploadFolder(true)}>
-            폴더 업로드 (백엔드 구현 필요)
-          </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setIsOpenUploadZip(true)}>폴더 압축파일 업로드</DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setIsOpenTestUpload(true)}>
-            테스트 업로드 (PresignedUrl 이용)
-          </DropdownMenuItem>{' '}
-          {/* 새로운 메뉴 항목 추가 */}
         </DropdownMenuContent>
       </DropdownMenu>
 
-      {/* 기존 Dialogs */}
       <Dialog
-        open={isOpenUploadFile}
-        onOpenChange={setIsOpenUploadFile}
-      >
-        <DialogTrigger asChild></DialogTrigger>
-        <DialogContent className="max-w-2xl">
-          <DialogHeader title={fileCount > 0 ? `파일 업로드 (${fileCount})` : '파일 업로드'} />
-          <ImageUploadForm
-            onClose={handleCloseUploadFile}
-            onRefetch={onRefetch}
-            onFileCount={(fileCount: number) => setFileCount(fileCount)}
-            projectId={projectId}
-            folderId={folderId}
-            uploadImageZipMutation={uploadImageZipMutation}
-            uploadImageFolderFileMutation={uploadImageFolderFileMutation}
-            uploadImageFileMutation={uploadImageFileMutation}
-            uploadImageFolderMutation={uploadImageFolderMutation}
-          />
-        </DialogContent>
-      </Dialog>
-
-      <Dialog
-        open={isOpenUploadPresigned}
-        onOpenChange={setIsOpenUploadPresigned}
+        open={isOpenUpload}
+        onOpenChange={setIsOpenUpload}
       >
         <DialogTrigger asChild></DialogTrigger>
         <DialogContent className="max-w-2xl">
           <DialogHeader
-            title={presignedCount > 0 ? `파일 업로드 PreSigned (${presignedCount})` : '파일 업로드 PreSigned'}
+            title={
+              fileCount > 0
+                ? `파일 업로드 (${fileCount})`
+                : uploadType === 'file'
+                  ? '파일 업로드'
+                  : uploadType === 'folder'
+                    ? '폴더 업로드'
+                    : '압축 파일 업로드'
+            }
           />
-          <ImageUploadPresignedForm
-            onClose={() => setIsOpenUploadPresigned(false)}
-            onRefetch={onRefetch}
-            onFileCount={(fileCount: number) => setPresignedCount(fileCount)}
-            projectId={projectId}
-            folderId={folderId}
-          />
-        </DialogContent>
-      </Dialog>
-
-      <Dialog
-        open={isOpenUploadFolderFile}
-        onOpenChange={setIsOpenUploadFolderFile}
-      >
-        <DialogTrigger asChild></DialogTrigger>
-        <DialogContent className="max-w-2xl">
-          <DialogHeader title="폴더 업로드 (파일 업로드 API 이용)" />
-          <ImageUploadForm
-            onClose={handleCloseUploadFolderFile}
-            onRefetch={onRefetch}
-            onFileCount={handleFileCount}
-            projectId={projectId}
-            folderId={folderId}
-            isFolderUpload={true}
-            uploadImageZipMutation={uploadImageZipMutation}
-            uploadImageFolderFileMutation={uploadImageFolderFileMutation}
-            uploadImageFileMutation={uploadImageFileMutation}
-            uploadImageFolderMutation={uploadImageFolderMutation}
-          />
-        </DialogContent>
-      </Dialog>
-
-      <Dialog
-        open={isOpenUploadFolder}
-        onOpenChange={setIsOpenUploadFolder}
-      >
-        <DialogTrigger asChild></DialogTrigger>
-        <DialogContent className="max-w-2xl">
-          <DialogHeader title="폴더 업로드 (백엔드 구현 필요)" />
-          <ImageUploadForm
-            onClose={handleCloseUploadFolder}
-            onRefetch={onRefetch}
-            onFileCount={handleFileCount}
-            projectId={projectId}
-            folderId={folderId}
-            isFolderBackendUpload={true}
-            uploadImageZipMutation={uploadImageZipMutation}
-            uploadImageFolderFileMutation={uploadImageFolderFileMutation}
-            uploadImageFileMutation={uploadImageFileMutation}
-            uploadImageFolderMutation={uploadImageFolderMutation}
-          />
-        </DialogContent>
-      </Dialog>
-
-      <Dialog
-        open={isOpenUploadZip}
-        onOpenChange={setIsOpenUploadZip}
-      >
-        <DialogTrigger asChild></DialogTrigger>
-        <DialogContent className="max-w-2xl">
-          <DialogHeader title="폴더 압축파일 업로드" />
-          <ImageUploadForm
-            onClose={handleCloseUploadZip}
-            onRefetch={onRefetch}
-            onFileCount={handleFileCount}
-            projectId={projectId}
-            folderId={folderId}
-            isZipUpload={true}
-            uploadImageZipMutation={uploadImageZipMutation}
-            uploadImageFolderFileMutation={uploadImageFolderFileMutation}
-            uploadImageFileMutation={uploadImageFileMutation}
-            uploadImageFolderMutation={uploadImageFolderMutation}
-          />
-        </DialogContent>
-      </Dialog>
-
-      {/* 테스트 업로드 Dialog */}
-      <Dialog
-        open={isOpenTestUpload}
-        onOpenChange={setIsOpenTestUpload}
-      >
-        <DialogTrigger asChild></DialogTrigger>
-        <DialogContent className="max-w-2xl">
-          <DialogHeader title="테스트 업로드 (PresignedUrl 이용)" />
           <ImagePreSignedForm
-            onClose={handleCloseTestUpload}
+            onClose={handleCloseUpload}
             onRefetch={onRefetch}
-            onFileCount={(fileCount: number) => setFileCount(fileCount)}
+            onFileCount={handleFileCount}
             projectId={projectId}
             folderId={folderId}
-            uploadType="folder" // zip flie 가능
+            uploadType={uploadType}
           />
         </DialogContent>
       </Dialog>
